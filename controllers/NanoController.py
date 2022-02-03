ecc5a5417cccc9b1425ed88c464e3aa24ee92c18
man90 <man90@tuta.io>
2022-02-03 10:23:41 +0100

    Get rid of prefixing in NanoDB

--- controllers/NanoController.py
+++ controllers/NanoController.py
@@ -8,16 +8,16 @@ class NanoController():
 	def __init__(self, addr):
 		self.addr = addr
 
-	def _create_address(self, table, identifier):
-		return "{}/{}:{}".format(self.addr, table, identifier)
+	def _create_address(self, identifier):
+		return "{}/{}".format(self.addr, identifier)
 
 	def create_one(self, identifier, data):
-		addr = self._create_address("bench", identifier)
+		addr = self._create_address(identifier)
 		# Returns True if record was created, otherwise False
 		return requests.put(addr, data=json.dumps(data)).ok
 
 	def read_one(self, identifier):
-		addr = self._create_address("bench", identifier)
+		addr = self._create_address(identifier)
 		# Returns data read from database as parsed JSON
 		return requests.get(addr).json()
 
@@ -26,6 +26,6 @@ class NanoController():
 		return self.create_one(identifier, data)
 
 	def delete_one(self, identifier):
-		addr = self._create_address("bench", identifier)
+		addr = self._create_address(identifier)
 		# Returns True if record was deleted, otherwise False
 		return requests.delete(addr).ok
